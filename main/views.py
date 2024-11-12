fc2559a694d3bce8e2fedec6b87b6dea78a46e54
Mohamed Ismail <mohamed.g.ismail@orange.com>
2024-11-12 22:38:47 +0200

    finish page 2

--- main/views.py
+++ main/views.py
@@ -8,11 +8,14 @@ from .models import Contact, JobRequest, News, ProjectRequest
 
 # Create your views here.
 
+
 class HomePageView(TemplateView):
-    template_name = 'main/home.html'
+    template_name = "main/home.html"
+
 
 def home_page_view(request):
-    return redirect('home-page')
+    return redirect("home-page")
+
 
 class PrivacyPageView(TemplateView):
     template_name = "main/privacy.html"
